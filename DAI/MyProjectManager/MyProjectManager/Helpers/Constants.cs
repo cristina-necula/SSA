203ced145d8f9d5ad06eb1ddbafffd9ff85ff3e8
cristina-necula <cristina.necula20@gmail.com>
2017-01-07 14:07:45 +0200

    removed DAI project

--- DAI/MyProjectManager/MyProjectManager/Helpers/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace MyProjectManager.Helpers
-{
-	public static class Constants
-	{
-		public static readonly String NOTICE = "notice";
-	}
-}
--- DAI/MyProjectManager/MyProjectManager/Models/TaskComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyProjectManager.Models
-{
-	public class TaskComponent
-	{
-		public int ID { get; set; }
-		public string Description { get; set; }
-		public int ImplementationTime { get; set; }
-	}
-}
